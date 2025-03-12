e8ead574227240116c4de0bc5f0b6051a97eb2b9
miguelMedinaCastro <miguelmedinadecastro@gmail.com>
2025-03-11 22:26:29 -0300

    update

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'inputUser.html'));
+});
+
+app.get('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
